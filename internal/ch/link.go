55141a056014f8303e465de3e3279ad50fedbb26
liwliwei <liwliwei@didiglobal.com>
2023-05-18 20:58:38 +0800

    soft link

--- internal/ch/link.go
+++ internal/ch/link.go
@@ -4,13 +4,13 @@
 package ch
 
 /*
-#cgo linux LDFLAGS: -L${SRCDIR}/../../clibs/hs_centos72_install/lib64/ -lpcre
-#cgo linux CFLAGS: -I${SRCDIR}/../../clibs/hs_centos72_install/include/hs
-#cgo linux LDFLAGS: -L${SRCDIR}/../../clibs/hs_centos72_install/lib64/ -lhs
-#cgo linux LDFLAGS: -L${SRCDIR}/../../clibs/hs_centos72_install/lib64/ -lchimera
-#cgo darwin LDFLAGS: -L${SRCDIR}/../../clibs/hs_darwin_install/lib/ -lpcre
-#cgo darwin CFLAGS: -I${SRCDIR}/../../clibs/hs_darwin_install/include/hs
-#cgo darwin LDFLAGS: -L${SRCDIR}/../../clibs/hs_darwin_install/lib/ -lhs
-#cgo darwin LDFLAGS: -L${SRCDIR}/../../clibs/hs_darwin_install/lib/ -lchimera
+#cgo linux LDFLAGS: -L${SRCDIR}/clibs/hs_centos72_install/lib64/ -lpcre
+#cgo linux CFLAGS: -I${SRCDIR}/clibs/hs_centos72_install/include/hs
+#cgo linux LDFLAGS: -L${SRCDIR}/clibs/hs_centos72_install/lib64/ -lhs
+#cgo linux LDFLAGS: -L${SRCDIR}/clibs/hs_centos72_install/lib64/ -lchimera
+#cgo darwin LDFLAGS: -L${SRCDIR}/clibs/hs_darwin_install/lib/ -lpcre
+#cgo darwin CFLAGS: -I${SRCDIR}/clibs/hs_darwin_install/include/hs
+#cgo darwin LDFLAGS: -L${SRCDIR}/clibs/hs_darwin_install/lib/ -lhs
+#cgo darwin LDFLAGS: -L${SRCDIR}/clibs/hs_darwin_install/lib/ -lchimera
 */
 import "C"
