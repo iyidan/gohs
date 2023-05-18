ffb06f00f15d644da09f74cce2b36ae838280f9b
liwliwei <liwliwei@didiglobal.com>
2023-05-18 21:15:01 +0800

    tes

--- internal/hs/link.go
+++ internal/hs/link.go
@@ -1,10 +1,10 @@
 package hs
 
 /*
-#cgo linux CFLAGS: -I${SRCDIR}/clibs/hs_centos72_install/include/hs
-#cgo linux LDFLAGS: -L${SRCDIR}/clibs/hs_centos72_install/lib64/ -lhs
-#cgo darwin CFLAGS: -I${SRCDIR}/clibs/hs_darwin_install/include/hs
-#cgo darwin LDFLAGS: -L${SRCDIR}/clibs/hs_darwin_install/lib/ -lhs
+#cgo linux CFLAGS: -I../../clibs/hs_centos72_install/include/hs
+#cgo linux LDFLAGS: -L../../clibs/hs_centos72_install/lib64/ -lhs
+#cgo darwin CFLAGS: -I../../clibs/hs_darwin_install/include/hs
+#cgo darwin LDFLAGS: -L../../clibs/hs_darwin_install/lib/ -lhs
 #cgo linux LDFLAGS: -lm -lstdc++
 */
 import "C"
