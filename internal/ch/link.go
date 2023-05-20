28458826d3f74ff2d246363f29b3bd3fc1089f6d
liwliwei <liwliwei@didiglobal.com>
2023-05-21 02:48:39 +0800

    mod linux cgo linux LDFLAGS

--- internal/ch/link.go
+++ internal/ch/link.go
@@ -7,7 +7,8 @@ package ch
 #cgo linux LDFLAGS: -L../../clibs/hs_centos72_install/lib64/ -lpcre
 #cgo linux CFLAGS: -I../../clibs/hs_centos72_install/include/hs
 #cgo linux LDFLAGS: -L../../clibs/hs_centos72_install/lib64/ -lhs
-#cgo linux LDFLAGS: -L../../clibs/hs_centos72_install/lib64/ -lchimera
+#cgo linux LDFLAGS: -L../../clibs/hs_centos72_install/lib64/ -lchimera -lpcre -lhs -lstdc++ -lm -lc -lgcc_s -lgcc
+#cgo linux LDFLAGS: -lm -lstdc++ -lpcre
 #cgo darwin LDFLAGS: -L../../clibs/hs_darwin_install/lib/ -lpcre
 #cgo darwin CFLAGS: -I../../clibs/hs_darwin_install/include/hs
 #cgo darwin LDFLAGS: -L../../clibs/hs_darwin_install/lib/ -lhs
